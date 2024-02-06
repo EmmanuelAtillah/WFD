ec0495efa4e7e820a29233767a6eede88de4f5a5
EmmanuelAtillah <156588035+EmmanuelAtillah@users.noreply.github.com>
2024-02-06 13:40:48 +0000

    Add files via upload

---
+++ script/store.js
@@ -0,0 +1,37 @@
+/*<footer class="footer">
+<div class="container">
+<div class="row">
+  <div class="footer-col">
+    <h4>Home</h4>
+    <ul>
+      <li><a href="#">about us</a></li>
+      <li><a href="#">our service</a></li>
+      <li><a href="#">privacy policy</a></li>
+    </ul>
+  </div>
+  <div class="footer-col">
+    <h4>Get help</h4>
+    <ul>
+      <li><a href="#">courses</a></li>
+      <li><a href="#">academic term</a></li>
+      <li><a href="#">time table</a></li>
+    </ul>
+  </div>
+  <div class="footer-col">
+    <h4>Follow us</h4>
+    <div class="social-links">
+      <a href="#"><i class="fab fa-facebook-f"></i></a>
+      <a href="#"><i class="fab fa-twitter"></i></a>
+      <a href="#"><i class="fab fa-instagram"></i></a>
+      <a href="#"><i class="fab fa-linkedin-in"></i></a>
+
+    </div>
+  </div>
+  </div>
+</div>
+</div>
+</footer>
+
+    </body>
+  
+  </html>
